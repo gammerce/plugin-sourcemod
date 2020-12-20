7835022923cd5fa4aa86f6145c2e88d40703e693
Michał Budziak <budziam@users.noreply.github.com>
2020-12-20 13:23:16 +0100

    Scripting (#4)

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ "$#" -lt 1 ]; then
+    echo "Usage: ./build.sh <version> <mode>"
+    exit 1
+fi
+
+BUILD_VERSION=$1
+MODE=${2:-watch}
+DIR="$( cd "$( dirname $( dirname "${BASH_SOURCE[0]}" ) )" && pwd )"
+
+# PREPARE
+mkdir -p "$DIR/plugins"
+chmod 777 "$DIR/plugins"
+rm -f $DIR/plugins/*
+
+docker run \
+    --rm \
+    -v "$DIR/plugins":/tmp/builds \
+    -v "$DIR/scripting":/tmp/sources \
+    -v "$DIR/scripting/include/shopsms.inc":/tmp/addons/sourcemod/scripting/include/shopsms.inc \
+    -v "$DIR/scripting/include/shopsms_const.inc":/tmp/addons/sourcemod/scripting/include/shopsms_const.inc \
+    budziam/sourcemod-builder:${BUILD_VERSION} \
+    ${MODE}
